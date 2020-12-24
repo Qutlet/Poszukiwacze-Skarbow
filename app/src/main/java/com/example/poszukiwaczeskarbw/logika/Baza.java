1ae337e7c66b24d9de8b5fc3e26fdb7c73e46cbe
Qutlet <maciej.bigos@onet.com.pl>
2020-12-24 02:09:40 +0100

    zmiany zmiany

--- app/src/main/java/com/example/poszukiwaczeskarbw/logika/Baza.java
+++ app/src/main/java/com/example/poszukiwaczeskarbw/logika/Baza.java
@@ -1,11 +1,12 @@
 /*
  * Created by Maciej Bigos & Jan Stawiński & Michalina Olczyk
  * Copyright (c) 2020. All rights reserved
- * Last modified 23.12.20 20:28
+ * Last modified 24.12.20 02:08
  */
 
 package com.example.poszukiwaczeskarbw.logika;
 
+import android.annotation.SuppressLint;
 import android.os.StrictMode;
 
 import java.sql.Connection;
@@ -16,6 +17,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Baza {
+    private Uzytkownik uzytkowniczek = Uzytkownik.getUzytkowniczek();
     private static Baza bazunia = new Baza();
     public static Baza getBaza() {
         return bazunia;
@@ -29,7 +31,7 @@ public class Baza {
         StrictMode.ThreadPolicy politka = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(politka);
         try {
-            final String parametryPoloczenia = "jdbc:sqlserver://poszukiwaczeskarbow.database.windows.net:1433;database=PoszukiwaczeSkarbow;user=qutelt@poszukiwaczeskarbow;password=YmsupDsk7pmTCU9;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
+            @SuppressLint("AuthLeak") final String parametryPoloczenia = "jdbc:sqlserver://poszukiwaczeskarbow.database.windows.net:1433;database=PoszukiwaczeSkarbow;user=qutelt@poszukiwaczeskarbow;password=YmsupDsk7pmTCU9;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
             final String sterownikSQL = "net.sourceforge.jtds.jdbc.Driver";
             Class.forName(sterownikSQL);
             polaczenie = DriverManager.getConnection(parametryPoloczenia);
@@ -58,22 +60,20 @@ public class Baza {
      * dodaje nowego uzytkownika do systemu
      * @param imie - imie
      * @param nazwisko - nazwisko
-     * @param nazwaUzytkownika -nazwa uzytkownika todo usunac w razie potrzeby
      * @param dataUrodzenia - data urodzenia
      * @param email - email
      * @return 0 - operacja zakonczona powodzenie / 1 - nazwa uzytkownika zajeta / 100 - wystapil blad podczas operacji
      */
-    public int dodajNowegoUzytkownika(String imie, String nazwisko, String nazwaUzytkownika,String haslo ,String dataUrodzenia, String email){//TODO: czy jakąś nazwe użytkownika tu dajemy?
-        if (sprawdzDostepnoscNazwyUzytkownika(nazwaUzytkownika) == 0){ //todo jezeli nie dodajemy nazwy uztkownika usunac tego if'a oraz metode sprawdzDostepnoscNazwyUzytkownika(String nazwaUzytkownika), nastepnie przerobic tą metode aby zwracala boolean'a (true - ok/false - not ok)
+    public int dodajNowegoUzytkownika(String imie, String nazwisko,String haslo ,String dataUrodzenia, String email){
+        if (sprawdzCzyPodanyAdresEmailJestJuzWUzyciu(email) == 0){
             polacz();
-            String zapytanie = "insert into Uzytkownicy (imie,nazwisko,nazwaUzytkownika,haslo,dataUrodzenia,email) values (?,?,?,?,?,?);"; //todo z tego zapytania i ponizszego komunikatu rowniez w ewentualnosci usunac nazweUzytkownika
+            String zapytanie = "insert into Uzytkownicy (imie,nazwisko,haslo,dataUrodzenia,email) values (?,?,?,?,?);";
             try (PreparedStatement komunikat = polaczenie.prepareStatement(zapytanie)){
                 komunikat.setString(1,imie);
                 komunikat.setString(2,nazwisko);
-                komunikat.setString(3,nazwaUzytkownika);
-                komunikat.setString(4,haslo);
-                komunikat.setDate(5,Date.valueOf(dataUrodzenia));//todo 1) emm upewnic sie czy to dziala 2) foramt daty podanej przy rejestracji to DD-MM-RRRR
-                komunikat.setString(6,email);
+                komunikat.setString(3,haslo);
+                komunikat.setDate(4,Date.valueOf(dataUrodzenia));//todo 1) emm upewnic sie czy to dziala 2) foramt daty podanej przy rejestracji to RRRR-MM-DD
+                komunikat.setString(5,email);
                 komunikat.execute();
                 rozlacz();
                 return 0;
@@ -87,17 +87,17 @@ public class Baza {
     }
 
     /**
-     * sprawdza czy uzytkownik o podanej nazwie uzytkownika znajduję się już w systemie
-     * @param nazwaUzytkownika - podana nazwa uzytkownika
+     * sprawdza czy uzytkownik z podanym adresem email znajduję się już w systemie
+     * @param email - podany adres email
      * @return 1 - jezeli nazwa wystepuje juz w sytemie / 0 - jezeli nazwa jest dostepna / 100 - blad operacji
      */
-    private int sprawdzDostepnoscNazwyUzytkownika(String nazwaUzytkownika){
+    private int sprawdzCzyPodanyAdresEmailJestJuzWUzyciu(String email){
         polacz();
-        String zapytanie = "select nazwaUzytkownika from Uzytkownicy";
+        String zapytanie = "select email from Uzytkownicy";
         try (PreparedStatement komunikat = polaczenie.prepareStatement(zapytanie)){
             ResultSet tablicaWynikow = komunikat.executeQuery();
             while (tablicaWynikow.next()) {
-                if (tablicaWynikow.getString(1).equals(nazwaUzytkownika)){
+                if (tablicaWynikow.getString(1).equals(email)){
                     rozlacz();
                     return 1;
                 }
@@ -113,20 +113,20 @@ public class Baza {
 
     /**
      * sprawdza dane logowania uzytkownika
-     * @param nazwaUzytkownika - podana nazwa uzytkownika
+     * @param email - podany adres email
      * @param haslo - haslo
      * @return 0 - dane poprawne / 1 - dane niepoprawne / 100 - blad operacji
      */
-    public int sprawdzDaneLogowaniaUzytkownika(String nazwaUzytkownika /*String email */, String haslo){
-        //todo w zaleznosci od podjetych wyzej decyzji (logowac sie za pomoca nazwyUzytkownika badz adresuu emial) zmienic odpowiednie wartosci w tej metodzie, oraz dokumentacje powyzej
+    public int weryfikujDaneLogowaniaUzytkownika(String email , String haslo){
         polacz();
-        String zapytanie = "select nazwaUzytkownika,haslo from Uzytkownicy"; //String zapytanie = "select email,haslo from Uzytkownicy";
+        String zapytanie = "select email,haslo from Uzytkownicy";
         try (PreparedStatement komunikat = polaczenie.prepareStatement(zapytanie)){
             ResultSet tablicaWynikow = komunikat.executeQuery();
             while (tablicaWynikow.next()) {
-                if (tablicaWynikow.getString(1).equals(nazwaUzytkownika)){  //if (tablicaWynikow.getString(1).equals(email)){
+                if (tablicaWynikow.getString(1).equals(email)){
                     if (tablicaWynikow.getString(2).equals(haslo)){
                         rozlacz();
+                        pobierzWszystkieDaneUzytkownikaZaWyjatkiemHaslaOrazWstawJeDoKlasyUzytkownik(email);
                         return 0;
                     }
                 }
@@ -138,5 +138,45 @@ public class Baza {
         }
         rozlacz();
         return 1;
+    }
+
+    /**
+     * wykonuje sie automatycznie, podczas weryfikacji danych logowania w metodzie weryfikujDaneLogowaniaUzytkownika(String email , String haslo)
+     * pobiera dane uzytkownika z systemu takie jak:
+     * id, imie, nazwisko, data urodzenie, email
+     * oraz jego statystyki:
+     * odnalezione i umieszczone skarby oraz przebyte km
+     * nastepnie zapisuje te dane w klasie Uytkownik w celu ulatwienia dalszego dostepu do nich
+     * jezeli podczas operacji wystopi blad do danych uzytkownika zostana przypisane przykladowe dane
+     * @param email - parametr logowania to na jego podstawie szukany jest uzytkownik w systemie
+     */
+    private void pobierzWszystkieDaneUzytkownikaZaWyjatkiemHaslaOrazWstawJeDoKlasyUzytkownik(String email){
+        polacz();
+        String zapytanie = "select idUzytkownika, imie, nazwisko, dataUrodzenia, email, odnalezioneSkarby, przebyteKM from Uzytkownicy where email = '" + email + "'";
+        try (PreparedStatement komunikat = polaczenie.prepareStatement(zapytanie)){
+            ResultSet tablicaWynikow = komunikat.executeQuery();
+            if (tablicaWynikow.next()) {
+              uzytkowniczek.setIdUzytkownika(tablicaWynikow.getInt(1));
+              uzytkowniczek.setImie(tablicaWynikow.getString(2));
+              uzytkowniczek.setNazwisko(tablicaWynikow.getString(3));
+              uzytkowniczek.setDataUrodzenia(tablicaWynikow.getDate(4));
+              uzytkowniczek.setEmail(email);
+              uzytkowniczek.setOdnalezioneSkarby(6);
+              uzytkowniczek.setPrzebyteKM(7);
+            }
+        }catch (SQLException e){
+            //TODO: dodac ewentualna obsluge wyjatku
+            e.printStackTrace();
+        }
+        zapytanie = "slect count(*) from Mapy where idAutora = " + uzytkowniczek.getIdUzytkownika();
+        try (PreparedStatement komunikat = polaczenie.prepareStatement(zapytanie)){
+            ResultSet tablicaWynikow = komunikat.executeQuery();
+            if (tablicaWynikow.next()) {
+                uzytkowniczek.setUmieszczoneSkarby(tablicaWynikow.getInt(1));
+            }
+        }catch (SQLException e){
+            //TODO: dodac ewentualna obsluge wyjatku
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/poszukiwaczeskarbw/logika/Uzytkownik.java
+++ app/src/main/java/com/example/poszukiwaczeskarbw/logika/Uzytkownik.java
@@ -1,10 +1,90 @@
 /*
  * Created by Maciej Bigos & Jan Stawiński & Michalina Olczyk
  * Copyright (c) 2020. All rights reserved
- * Last modified 23.12.20 13:20
+ * Last modified 24.12.20 02:06
  */
 
 package com.example.poszukiwaczeskarbw.logika;
 
+import java.sql.Date;
+
 public class Uzytkownik {
+    private int idUzytkownika = 1;
+    private String imie = "Poszukiwacz";
+    private String nazwisko = "Skarbow";
+    private Date dataUrodzenia = Date.valueOf("1999-01-01");
+    private String email = "poszukiwaczSkarbow@example.com";
+    private int przebyteKM = 0;
+    private int odnalezioneSkarby = 0;
+    private int umieszczoneSkarby = 0;
+    private static Uzytkownik uzytkowniczek = new Uzytkownik();
+
+    public static Uzytkownik getUzytkowniczek() {
+        return uzytkowniczek;
+    }
+
+    public int getIdUzytkownika() {
+        return idUzytkownika;
+    }
+
+    public void setIdUzytkownika(int idUzytkownika) {
+        this.idUzytkownika = idUzytkownika;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public Date getDataUrodzenia() {
+        return dataUrodzenia;
+    }
+
+    public void setDataUrodzenia(Date dataUrodzenia) {
+        this.dataUrodzenia = dataUrodzenia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPrzebyteKM() {
+        return przebyteKM;
+    }
+
+    public void setPrzebyteKM(int przebyteKM) {
+        this.przebyteKM = przebyteKM;
+    }
+
+    public int getOdnalezioneSkarby() {
+        return odnalezioneSkarby;
+    }
+
+    public void setOdnalezioneSkarby(int odnalezioneSkarby) {
+        this.odnalezioneSkarby = odnalezioneSkarby;
+    }
+
+    public int getUmieszczoneSkarby() {
+        return umieszczoneSkarby;
+    }
+
+    public void setUmieszczoneSkarby(int umieszczoneSkarby) {
+        this.umieszczoneSkarby = umieszczoneSkarby;
+    }
+
 }
